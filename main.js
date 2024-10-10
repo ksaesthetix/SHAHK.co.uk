da737425c0019aaa08ff247aba5e8f316b49e62f
Kieran Shah <75956352+ksaesthetix@users.noreply.github.com>
2024-10-10 21:33:11 +0000

    Changes to main files

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-// change nav style on scroll
-
+// Change nav style on scroll
 window.addEventListener('scroll', () => {
     const nav = document.querySelector('nav');
     if (window.scrollY > 0) {
@@ -7,12 +6,32 @@ window.addEventListener('scroll', () => {
     } else {
         nav.classList.remove('window-scrolled');
     }
-})
+});
 
-// contact button circular text 
+// Circular text for contact button
 const textButtons = document.querySelectorAll('.contact_btn');
 
 textButtons.forEach(textButton => {
-    let text = textButton.querySelector('p')
-    text.innerHTML = text.innerHTML.split('').map((character, index) => `<span style="transform: rotate(${index * 12}deg)">${character}</span>`).join()
-})
+    let text = textButton.querySelector('p');
+    text.innerHTML = text.innerHTML.split('').map((character, index) => 
+        `<span style="transform: rotate(${index * 12}deg)">${character}</span>`
+    ).join('');
+});
+
+// Responsive Nav Bar for mobile
+const navToggleOpen = document.getElementById('nav_toggle-open');
+const navToggleClose = document.getElementById('nav_toggle-close');
+const navLinks = document.querySelector('.nav_links');
+
+navToggleOpen.addEventListener('click', () => {
+    navLinks.style.display = 'flex';
+    navToggleOpen.style.display = 'none';
+    navToggleClose.style.display = 'block';
+});
+
+navToggleClose.addEventListener('click', () => {
+    navLinks.style.display = 'none';
+    navToggleClose.style.display = 'none';
+    navToggleOpen.style.display = 'block';
+});
+
