2888a627d4bcdbfc2feb15c56deb61636084ecc3
Kieran Shah <75956352+ksaesthetix@users.noreply.github.com>
2024-10-10 22:04:03 +0100

    Update main.js

--- root/main.js
+++ root/main.js
@@ -16,3 +16,20 @@ textButtons.forEach(textButton => {
     let text = textButton.querySelector('p')
     text.innerHTML = text.innerHTML.split('').map((character, index) => `<span style="transform: rotate(${index * 12}deg)">${character}</span>`).join()
 })
+
+// Responsive Nav Bar for mobile
+const navToggleOpen = document.getElementById('nav_toggle-open');
+const navToggleClose = document.getElementById('nav_toggle-close');
+const navLinks = document.querySelector('.nav_links');
+
+navToggleOpen.addEventListener('click', () => {
+    navLinks.style.display = 'flex';
+    navToggleOpen.style.display = 'none';
+    navToggleClose.style.display = 'block';
+});
+
+navToggleClose.addEventListener('click', () => {
+    navLinks.style.display = 'none';
+    navToggleClose.style.display = 'none';
+    navToggleOpen.style.display = 'block';
+});
